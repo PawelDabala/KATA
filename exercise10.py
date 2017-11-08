a0329bf66b444fc39d9d12848149382b281b905b
Pawel Dabala <pdabala@gmail.com>
2017-11-08 09:56:38 +0100

    Add files via upload

---
+++ exercise10.py
@@ -0,0 +1,12 @@
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+
+    def is_worth_it(self):
+        return self.draft - (self.crew * 1.5) > 20
+
+
+EmptyShip = Ship(0,0)
+print(EmptyShip.is_worth_it())
+
